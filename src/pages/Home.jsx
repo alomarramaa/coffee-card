b4aa812909653022c3de95588390472cbe4a8326
Rama Al-Omar <alomar.ramaa@gmail.com>
2024-07-16 13:20:23 -0400

    3d model finally works :-)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,30 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
-import { OrthographicCamera } from '@react-three/drei'
-import useSpline from '@splinetool/r3f-spline'
-
-
-
+import { OrthographicCamera } from "@react-three/drei";
+import useSpline from "@splinetool/r3f-spline";
+import App from "../models/CoffeeCorner";
 
 const Home = () => {
-  return <section className="w-full h-screen relative">
-    <Canvas className="w-full h-screen bg-transparent" camera={{near:0.1, far:1000}}>
-      <Suspense fallback={<Loader />}>
+  return (
+    <section className="w-full h-screen relative">
+      {/* <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight /> */}
 
-      </Suspense>
-    </Canvas>
-     </section>;
+      <App />
+      {/*         
+        </Suspense>
+      </Canvas> */}
+    </section>
+  );
 };
 
 export default Home;
