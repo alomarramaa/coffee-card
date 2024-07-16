476ccf4f9ed6524417b364bb5005f77afa3fd8ce
Rama Al-Omar <alomar.ramaa@gmail.com>
2024-07-16 10:47:16 -0400

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+//build app here
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
