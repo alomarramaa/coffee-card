52c9691176d5673f4adef3383bb9212899d5123e
Rama Al-Omar <alomar.ramaa@gmail.com>
2024-07-16 11:08:36 -0400

    adding routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 
 //build app here
-import React from 'react'
+import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
+
+    //<main style={{ backgroundColor: '#663D26' }}>
+  <main className="bg-slate-300/20">
+    <Router>
+       <Navbar />
+            <Routes>
+                <Route path="/" element={'Home'} />
+                <Route path="/about" element={'About'}/>
+                <Route path="/syrups" element={'Syrups'}/>
+                <Route path="/drinks" element={'Drinks'}/>
+                <Route path="/contact" element={'Contact'}/>
+            </Routes>
+    </Router>
+    
+  </main>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+//Create Navbar component
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">RA</p>
+      </NavLink>
+
+
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive
+              ? //when you click on it, it'll turn brown. Otherwise, it's black
+                "text-amber-700"
+              : "text-black"
+          }>
+          About
+        </NavLink>
+
+        <NavLink
+          to="/syrups"
+          className={({ isActive }) =>
+            isActive ? "text-amber-700" : "text-black"
+          }
+        >
+          Syrups
+        </NavLink>
+
+        <NavLink to="/drinks" className={({ isActive }) => isActive ? "text-amber-700" : "text-black"
+          }>
+          Drinks
+        </NavLink>
+
+
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
